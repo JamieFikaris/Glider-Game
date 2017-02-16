116dd96225cb3d3981f0820c7b9f4c51888ec36d
Jamie Fikaris-Bruce <jfbchilly@hotmail.co.uk>
2017-02-16 01:32:04 +0000

    Revert "Skeleton xaml for main window"
    This reverts commit b6d92d5c73041504225d8f914767de7754b17c9b.

--- Glider Game/Glider Game/MainWindow.xaml.cs
+++ Glider Game/Glider Game/MainWindow.xaml.cs
@@ -10,9 +10,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Glider_Game.Main_Window.View
+namespace Glider_Game
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
